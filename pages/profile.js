f7c3408cf3c3216278f033815b3450e0eb46d818
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-05-30 13:38:01 +0700

    update: added protect route

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,27 @@
+import { getSession } from 'next-auth/client';
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session: session,
+    },
+  };
+};
 
 export default ProfilePage;
